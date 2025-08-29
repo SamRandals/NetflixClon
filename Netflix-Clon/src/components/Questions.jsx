e5d11f540acb71fb6594ca71efa7cc1b73f81615
SamRandals <samnanezedu@gmail.com>
2025-08-29 16:49:46 -0500

    Netflix Clon upload

---
+++ Netflix-Clon/src/components/Questions.jsx
@@ -0,0 +1,36 @@
+
+import '../styles/QUestions.css'
+
+function PreguntasFrecuentes(){
+    
+    return(<>
+    
+   <section className="faq">
+  <details>
+    <summary>¿Qué es Netflix?</summary>
+    <p>Netflix es un servicio de streaming que ofrece series, películas, documentales y más.</p>
+  </details>
+
+  <details>
+    <summary>¿Cuánto cuesta Netflix?</summary>
+    <p>El precio depende del plan que elijas y puede variar según tu país.</p>
+  </details>
+
+  <details>
+    <summary>¿Dónde puedo ver Netflix?</summary>
+    <p>Puedes verlo en televisores, computadoras, teléfonos, tablets y consolas.</p>
+  </details>
+
+  <details>
+    <summary>¿Cómo cancelo?</summary>
+    <p>Puedes cancelar en cualquier momento desde la configuración de tu cuenta.</p>
+  </details>
+</section>
+
+    
+    
+    
+    </>)
+}
+
+export default PreguntasFrecuentes
